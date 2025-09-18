bc77ed2d06d50d4e65f26c1a8c7b47d37e1cfc12
marielle leivas de mattos <leivas.mari@gmail.com>
2025-09-18 17:34:43 -0300

    alteraçoes

--- Ecommerce/API/Program.cs
+++ Ecommerce/API/Program.cs
@@ -1,4 +1,5 @@
 using API.Models;
+using Microsoft.AspNetCore.Mvc;
 
 Console.Clear();
 var builder = WebApplication.CreateBuilder(args);
@@ -16,17 +17,47 @@ var app = builder.Build();
 //Requisições
 // - Endereço/URL
 // - Método HTTP
-app.MapGet("/", () => " API de Produtos ");
+// Métodos HTTP 
+// GET: BUSCAR DADOS DE UM SERVIDOR SEM MODIFICAR O RECURSO.
+// POST: ENVIAR DADOS PARA UM SERVIDOR PARA CRIAR UM NOVO RECURSO.
+// PUT: ATUALIZAR UM RECURSO EXISTENTE NO SERVIDOR.
+// DELETE: REMOVER UM RECURSO DO SERVIDOR.
 
+
+app.MapGet("/", () => " API de Produtos ");
 app.MapGet("/api/produto/listar",() =>
 {
-    return Results.Ok(produtos);
-});
-app.MapPost("/api/produto/cadastrar",(Produto produto) =>
-{
+// VALIDAR SE EXISTE ALGUMA COISA DENTRO DA LISTA 
+    if (produtos.Count > 0)
+    {
+        return Results.Ok(produtos);
+    }
+        return Results.BadRequest("lista vazia");
     
-   
+});
+app.MapPost("/api/produto/cadastrar", ([FromBody] Produto produto) =>
+{
+    foreach (Produto produtoCadastrado in produtos)
+    {
+        if (produtoCadastrado.Nome == produto.Nome)
+        {
+            return Results.Conflict("Produto já cadastrado");
+        }
+    }
+
     produtos.Add(produto);
+    return Results.Created("", produto);
+});
+app.MapGet("/api/produto/buscar", ([FromRoute] String nome) =>
+{
+    //exprexao lambda
+    // produto produto =produtos.FirstOrDefault( p => p.Nome == nome);
+    Produto? resultado = produtos.FirstOrDefault(p => p.Nome == nome);
+    if (resultado == null)
+    {
+        return Results.NotFound("Produto não encontrado");
+    }
+    return Results.Ok(resultado);
 });
 app.Run();
 
